358bc10654cd3578c631a811ea5735edf55a9a81
Callum Bonnyman <callum@wrux.com>
2022-11-24 17:34:46 +0900

    Added favicon.

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     </>
   );
 }
