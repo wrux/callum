236a3dfcb88268021f3d44ffbdb6ca3e788af53e
Callum <callumbonnyman@me.com>
2022-11-25 13:54:10 +0900

    Added SEO data in Sanity Studio. (#20)

--- src/studio/schemas/documents/post.ts
+++ src/studio/schemas/documents/post.ts
@@ -4,11 +4,23 @@ const schema = defineType({
   name: 'post',
   type: 'document',
   title: 'Post',
+  groups: [
+    {
+      name: 'content',
+      title: 'Content',
+      default: true,
+    },
+    {
+      name: 'metadata',
+      title: 'SEO & metadata',
+    },
+  ],
   fields: [
     defineField({
       name: 'title',
       type: 'string',
       title: 'Title',
+      group: 'content',
       validation: (rule) => rule.required(),
     }),
     defineField({
@@ -19,6 +31,7 @@ const schema = defineType({
         source: 'title',
         maxLength: 96,
       },
+      group: 'content',
       validation: (rule) => rule.required(),
     }),
     defineField({
@@ -28,36 +41,49 @@ const schema = defineType({
       options: {
         hotspot: true,
       },
+      group: 'content',
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'countries',
       type: 'array',
       title: 'Countries',
       of: [{ type: 'reference', to: { type: 'country' } }],
+      group: 'content',
     }),
     defineField({
       name: 'publishedAt',
       type: 'datetime',
       title: 'Published at',
+      group: 'content',
       validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'excerpt',
       type: 'text',
       title: 'Excerpt',
+      group: 'content',
     }),
     // TODO: Remove once no longer needed...
     defineField({
       name: 'body',
       title: 'Body',
       type: 'blockContent',
+      group: 'content',
     }),
-    {
+    defineField({
       name: 'content',
       title: 'Content',
       type: 'array',
       of: [{ type: 'imageBlock' }, { type: 'textBlock' }],
-    },
+      group: 'content',
+    }),
+    defineField({
+      name: 'seo',
+      title: 'SEO',
+      type: 'seo',
+      group: 'metadata',
+    }),
   ],
   preview: {
     select: {
