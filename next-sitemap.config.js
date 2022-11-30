d5e1cd7331263061579a9b2545df8774dd77b61b
Callum Bonnyman <callum@wrux.com>
2022-11-30 17:32:19 +0900

    Added sitemap and robots generation.

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL || 'https://callum.co.uk',
+  generateRobotsTxt: true,
+  sitemapSize: 1000,
+};
+
+module.exports = config;
