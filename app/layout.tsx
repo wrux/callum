ecf2987c631e1cefc8b0a5dfe8e651b8f32f6593
Callum Bonnyman <callum@wrux.com>
2022-11-30 17:54:33 +0900

    fixup! Added Plausible tracking script.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@fontsource/inter/variable.css';
+import Script from 'next/script';
 import 'styles/main.css';
 
 export default function RootLayout({
@@ -9,6 +10,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+
+      <Script
+        src="/stats/js/script.js"
+        strategy="afterInteractive"
+        data-api="/stats/api/event"
+        data-domain="callum.co.uk"
+      />
     </html>
   );
 }
