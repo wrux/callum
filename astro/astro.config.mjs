3507e512711a5a2df4389a4dc3591e67df55fa97
Callum Bonnyman <callum@wrux.com>
2023-09-01 15:31:09 +0900

    Added basic pages.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://callum.co.uk',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    sanity({
+      // projectId: import.meta.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+      // dataset: import.meta.env.NEXT_PUBLIC_SANITY_DATASET,
+      // apiVersion: import.meta.env.NEXT_PUBLIC_SANITY_API_VERSION,
+      projectId: 'kx8fng11',
+      dataset: 'staging',
+      // apiVersion: import.meta.env.NEXT_PUBLIC_SANITY_API_VERSION,
+      useCdn: false,
+    }),
+    react(),
+  ],
 });
--- astro/src/components/index.js
+++ astro/src/components/index.js
@@ -1,3 +1,6 @@
+export { default as ArticleTeaser } from './ArticleTeaser.astro';
+export { default as BaseHead } from './BaseHead.astro';
+export { default as CountryList } from './CountryList.astro';
 export { default as SiteFooter } from './SiteFooter.astro';
 export { default as SiteHeader } from './SiteHeader.astro';
 export { default as Wordmark } from './Wordmark.astro';
