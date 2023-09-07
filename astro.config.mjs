7b0745024c52caa660a0a5c7acb364185f2b529d
Callum Bonnyman <callum@wrux.com>
2023-09-07 16:14:31 +0900

    Removed old sample markdown content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import sanity from '@sanity/astro';
@@ -12,13 +11,9 @@ export default defineConfig({
     domains: ['cdn.sanity.io'],
   },
   integrations: [
-    mdx(),
     sitemap(),
     tailwind(),
     sanity({
-      // projectId: import.meta.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-      // dataset: import.meta.env.NEXT_PUBLIC_SANITY_DATASET,
-      // apiVersion: import.meta.env.NEXT_PUBLIC_SANITY_API_VERSION,
       projectId: 'kx8fng11',
       dataset: 'staging',
       // apiVersion: import.meta.env.NEXT_PUBLIC_SANITY_API_VERSION,
