bdb15f64806d38953037ca0a4826aa7c1e5206be
Callum Bonnyman <callum@wrux.com>
2023-10-07 15:44:15 +0900

    Restored imported config vars.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
+import { dataset, projectId } from './src/sanity/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,8 +17,8 @@ export default defineConfig({
   integrations: [
     react(),
     sanity({
-      projectId: 'kx8fng11',
-      dataset: 'staging',
+      projectId,
+      dataset,
       useCdn: false,
       studioBasePath: '/studio',
     }),
