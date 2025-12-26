f3d6bf1a0be8b282a652c25cbb4ffc9d6815398c
Callum Bonnyman <callum@wrux.com>
2025-12-26 12:08:51 +0300

    Remvoed type components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 export { default as ArticleTeaser } from './ArticleTeaser.astro';
 export { default as DateTime } from './DateTime.astro';
 export { default as FormattedDate } from './FormattedDate.astro';
-export { default as Heading } from './Heading.astro';
 export { default as Logo } from './Logo.astro';
 export { default as PortableText } from './PortableText.astro';
 export { default as SanityImage } from './SanityImage.astro';
 export { default as Section } from './Section.astro';
 export { default as TeaserGrid } from './TeaserGrid.astro';
-export { default as Text } from './Text.astro';
 export { default as Wordmark } from './Wordmark.astro';
